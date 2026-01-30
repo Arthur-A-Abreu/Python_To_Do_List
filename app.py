66b868d3340def5d3691833839e49185191f172d
Arthur Abreu <146493960+Arthur-A-Abreu@users.noreply.github.com>
2026-01-30 19:50:27 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+#Cria o app
+app = Flask(__name__)
+
+#Função para conectar ao banco de dados
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+#Ciar tabela se não existir
+def init_db():
+    conn = get_db_connection()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS task (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
+print("Banco e tabela verificados")
+
+
+#Define uma rota
+# GET and POST to do a Point Between the Client and the Server
+@app.route('/',methods=['GET', 'POST'])
+def home():
+    conn = get_db_connection()
+
+    if request.method == 'POST':
+        task = request.form['task']
+        conn.execute('INSERT INTO task (name) VALUES (?)', (task,))
+        conn.commit()
+        return redirect('/')
+
+    tasks = conn.execute('SELECT * FROM task').fetchall()
+    conn.close()
+
+
+    return render_template('index.html', tasks=tasks)
+
+
+#Deletar tarefas
+@app.route("/delete/<int:id>")
+def delete_task(id):
+    conn = get_db_connection()
+    conn.execute("DELETE FROM task WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
